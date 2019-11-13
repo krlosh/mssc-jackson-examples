d8a24a7ec6dcbecb2456602fed25ae3d99d41d06
carloshenche <carloshenche@metricool.com>
2019-11-13 13:04:31 +0100

    Setup kebab naming strategy

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -0,0 +1,23 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("kebab")
+@JsonTest
+class BeerDtoKebabTest extends  BaseTest{
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    void testKebab() throws JsonProcessingException {
+        BeerDto dto = this.getDto();
+
+        System.out.println(mapper.writeValueAsString(dto));
+    }
+}
