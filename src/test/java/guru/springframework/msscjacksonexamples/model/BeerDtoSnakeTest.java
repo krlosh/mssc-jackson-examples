870e8f2fdca276e39db55b919a13aee920769382
carloshenche <carloshenche@metricool.com>
2019-11-13 12:59:38 +0100

    Setup snake naming strategy by profile

---
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("snake")
+@JsonTest
+class BeerDtoSnakeTest extends  BaseTest{
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Test
+    void testSnake() throws JsonProcessingException {
+        BeerDto dto = this.getDto();
+
+        System.out.println(mapper.writeValueAsString(dto));
+    }
+}
